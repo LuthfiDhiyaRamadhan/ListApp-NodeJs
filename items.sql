6d4cff77a40c0bc48a1a65a6ea5f374a7a88c873
Luthfi Dhiya Ramadhan <luthfidhiyar16@gmail.com>
2022-09-02 18:24:11 +0700

    first commit

---
+++ items.sql
@@ -0,0 +1,5 @@
+INSERT INTO items(id,name) VALUES (1,'kentang');
+INSERT INTO items(id,name) VALUES (2,'wortel');
+INSERT INTO items(id,name) VALUES (3,'bawang');
+
+CREATE TABLE items (`id` int auto_increment, `name` text, primary key (id));
